d13d181b3226148b807d2b9d170a689f61a92342
ramandhiman256 <30542837+ramandhiman256@users.noreply.github.com>
2026-01-27 00:06:11 +0000

    Deploying to gh-pages from @ ramandhiman256/ramandhiman256.github.io@70d59ec7cd27a1e9b84bbafd5c9bb2d5916e9c50 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -436,7 +436,7 @@ ninja.data = [{
               window.location.href = "/books/";
             },},{id: "pages-cv",
           title: 'CV',
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Pages",handler: () => {
               window.location.href = "/cv/";
             },},{id: "pages-submenus",
